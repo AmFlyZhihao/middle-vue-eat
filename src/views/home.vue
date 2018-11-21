25ac4c13463766029a2e95c45a906880d0b3487b
yangzhihao <yangzhihao@miao.cn>
2018-11-21 10:53:52 +0800

    提交

---
+++ src/views/home.vue
@@ -0,0 +1,18 @@
+<template>
+	<div id="box">哈哈哈哈</div>
+</template>
+<script>
+	export default{
+		data(){
+			return{
+
+			}
+		},
+		created(){
+			
+		},
+		mounted(){
+			console.log($("#box").html("11111"))
+		}
+	}
+</script>
